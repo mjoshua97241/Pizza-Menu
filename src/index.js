568675b497cb7b781c9c85c22fd63110e83fe133
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-08-20 17:24:22 +0800

    Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -88,11 +88,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>;
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10} //for the number you must enclosed it with the JS mode "{}"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>;<span>{props.price + 3}</span>;
+      </div>
+    </div>
   );
 }
 
@@ -110,16 +132,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}. We're currently open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>;
-    </div>
   );
 }
 
@@ -163,4 +175,10 @@ SEPARATION OF CONCERNS? YES
     - Menu
     - Filters
     - Player
+
+PROPS {}
+  - HOW we passed data BETWEEN components (from parent to child components)
+  - a.k.a. communication channel
+  - you can pass through any data (arrays, objects, numbers or etc)
+  - FUNDAMENTAL things on the react
 */
